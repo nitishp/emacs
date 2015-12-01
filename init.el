36b986a3f3b191f70e0aac53db233f0475d27b01
Nitish Paradkar <nitishp@umich.edu>
2015-11-30 20:37:41 -0500

    Added some emacs config files

---
+++ init.el
@@ -0,0 +1,17 @@
+(global-linum-mode t) ;; set up line numbers
+(setq make-backup-files t) ;; Don't make backupFile~
+(setq-default indent-tabs-mode nil) ;; use spaces not tabs
+(setq-default tab-width 8) ;; Set the tab width to 4
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
