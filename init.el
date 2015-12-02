6d61346d8511c9d2e93c4d90d101e43fef3abf70
Nitish Paradkar <nitishp@umich.edu>
2015-12-02 15:58:02 -0500

    GOt the desert theme working

--- init.el
+++ init.el
@@ -1,7 +1,12 @@
 (global-linum-mode t) ;; set up line numbers
 (setq make-backup-files t) ;; Don't make backupFile~
 (setq-default indent-tabs-mode nil) ;; use spaces not tabs
-(setq-default tab-width 8) ;; Set the tab width to 4
+(setq-default tab-width 4) ;; Set the tab width to 4
+(setq-default c-basic-offset 4) ;; Set the offset for tabs to 4 for C, C++, Java files
+(setq c-default-style "linux") ;; Get rid of the annoying bracket misalignment
+
+;; Load the desert theme
+(load "~/.emacs.d/desert-theme.el")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
